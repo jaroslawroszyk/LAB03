2a65830b8ec8611d91ae71cb27277710d7799ca1
Jaroslaw Roszyk <roszyk.jarek@gmail.com>
2022-04-06 21:24:43 +0200

    Done task one

---
+++ LAB03/uts/Caesar_tests.cpp
@@ -0,0 +1,48 @@
+#include <gtest/gtest.h>
+#include "Caesar.h"
+#include <string>
+#include <tuple>
+
+using namespace Task1;
+
+class CaesarTests : public ::testing::Test
+{
+protected:
+
+    CaesarTests() =default;
+
+    ~CaesarTests() override = default;
+};
+
+static Caesar sut;
+
+TEST_F(CaesarTests, foo)
+{
+//    std::string str = "DO YOU WANNA JOIN";
+//    std::string_view key = "PRO";
+//    std::string output = "PTE EP SSLP";
+////    sut.ceaser(str, key);
+////    EXPECT_EQ(str,output);
+//    EXPECT_EQ(sut.ceaser(str, key),output);
+}
+
+class CaesarTestsParameterizedTestFixture : public ::testing::TestWithParam<std::tuple<std::string,std::string,std::string>>
+{};
+
+TEST_P(CaesarTestsParameterizedTestFixture,IsCorrectConversionForCaesarCipher)
+{
+    auto str = std::get<0>(GetParam());
+    auto key = std::get<1>(GetParam());
+    auto output = std::get<2>(GetParam());
+    EXPECT_EQ(output,sut.ceaser(str,key));
+}
+
+INSTANTIATE_TEST_SUITE_P(
+        CaesarTestsParameterizedTest,
+        CaesarTestsParameterizedTestFixture,
+        ::testing::Values(
+                std::make_tuple("ALA MA KOTA","PIES","PTE EP SSLP"),
+                std::make_tuple("WSB MA STUDENTOW","SESJA","OWT VA KXMMEFXGF"),
+                std::make_tuple("JESTEM PROGRAMISTA","CPP","LTHVTB RGDIGPOXHVP"),
+                std::make_tuple("WSB","KURCZAK","GMS"),
+                std::make_tuple("DO YOU WANNA JOIN","PRO","SF MDL KPEBP ACXE")));
